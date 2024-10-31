5de74f6f6cc6dd8dcae8865ac7ccfd55e590c9d6
shao <lanling_shao@126.com>
2024-10-31 20:02:07 +0800

    go generate ./...

--- cmd/hello_world/wire_gen.go
+++ cmd/hello_world/wire_gen.go
@@ -7,13 +7,13 @@
 package main
 
 import (
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
 	"hello_world/internal/biz"
 	"hello_world/internal/conf"
 	"hello_world/internal/data"
 	"hello_world/internal/server"
 	"hello_world/internal/service"
-	"github.com/go-kratos/kratos/v2"
-	"github.com/go-kratos/kratos/v2/log"
 )
 
 import (
