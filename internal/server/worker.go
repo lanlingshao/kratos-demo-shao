18a78c4c95b1ec1c7a5f48df11b9804837cb7d25
shao <lanling_shao@126.com>
2024-11-18 16:42:57 +0800

    定时任务增加DI

--- internal/server/worker.go
+++ internal/server/worker.go
@@ -3,10 +3,11 @@ package server
 import (
 	"context"
 	"fmt"
+	"github.com/go-kratos/kratos/v2/log"
 	"github.com/lanlingshao/kratos-demo-shao/internal/conf"
+	"github.com/lanlingshao/kratos-demo-shao/internal/service"
 	"github.com/xxl-job/xxl-job-executor-go"
 	"github.com/xxl-job/xxl-job-executor-go/example/task"
-	"log"
 )
 
 type CronWorker struct {
@@ -22,14 +23,15 @@ func (c *CronWorker) Stop(ctx context.Context) error {
 	return nil
 }
 
-func NewCronWorker(c *conf.Server) *CronWorker {
+func NewCronWorker(c *conf.Server, greeter *service.GreeterService, logger log.Logger) *CronWorker {
+	logg := log.NewHelper(log.With(logger, "module", "internal/server/worker"))
 	exec := xxl.NewExecutor(
 		xxl.ServerAddr(c.CronWorker.GetAddr()),
 		xxl.AccessToken(c.CronWorker.GetAccessToken()),   // 请求令牌(默认为空)
 		xxl.ExecutorIp(c.CronWorker.GetExecutorIp()),     // 可自动获取
 		xxl.ExecutorPort(c.CronWorker.GetExecutorPort()), // 默认9999（非必填）
 		xxl.RegistryKey(c.CronWorker.GetRegistryKey()),   // 执行器名称
-		xxl.SetLogger(&logger{}),                         // 自定义日志
+		xxl.SetLogger(&CronWorkerLogger{log: logg}),      // 自定义日志
 	)
 	exec.Init()
 	exec.Use(customMiddleware)
@@ -55,22 +57,30 @@ func customLogHandle(req *xxl.LogReq) *xxl.LogRes {
 }
 
 // xxl.Logger接口实现
-type logger struct{}
-
-func (l *logger) Info(format string, a ...interface{}) {
-	fmt.Println(fmt.Sprintf("自定义日志 - "+format, a...))
+type CronWorkerLogger struct {
+	log *log.Helper
 }
 
-func (l *logger) Error(format string, a ...interface{}) {
-	log.Println(fmt.Sprintf("自定义日志 - "+format, a...))
+func (l *CronWorkerLogger) Info(format string, a ...interface{}) {
+	var myInterface interface{} = format
+	v := myInterface.(string)
+	a = append(a, v)
+	l.log.Info(a...)
+}
+
+func (l *CronWorkerLogger) Error(format string, a ...interface{}) {
+	var myInterface interface{} = format
+	v := myInterface.(string)
+	a = append(a, v)
+	l.log.Error(a...)
 }
 
 // 自定义中间件
 func customMiddleware(tf xxl.TaskFunc) xxl.TaskFunc {
 	return func(cxt context.Context, param *xxl.RunReq) string {
-		log.Println("I am a middleware start")
+		fmt.Println("I am a middleware start")
 		res := tf(cxt, param)
-		log.Println("I am a middleware end")
+		fmt.Println("I am a middleware end")
 		return res
 	}
 }
